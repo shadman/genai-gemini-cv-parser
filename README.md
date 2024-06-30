ef0c693e97098bb2c01fc05faf53ab629e96d49a
Shadman Jamil <shadman.jamil@gmail.com>
2024-06-30 04:15:35 +0200

    added script for parsing

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# genai-gemini-cv-parser
+# GenAI Gemini CV Parser with Python
 Gemini API used to parse CV and identify candidate skills, expertise
+
+
+## Install dependencies
+> google.generativeai
+> pip install IPython
+
+
+## API Key
+Set Env or update API KEY on line number 9, with yours
+> GOOGLE_API_KEY="XXccss233XXcccsszX"
+
+
+## Run
+To get the output of our predefined input "What is the meaning of life?"
+> python genai.py
+
+
+## Input / Predefined Text
+Update the input parameter on line number 21, to test your required text/input value 
+
+
+# Get started
+- https://aistudio.google.com/app/apikey
+- https://colab.research.google.com/
+- https://colab.research.google.com/github/google/generative-ai-docs/blob/main/site/en/gemini-api/docs/get-started/python.ipynb#scrollTo=5b4Hkfj-pm3p
---
+++ genai.py
@@ -0,0 +1,56 @@
+import requests
+import os
+import textwrap
+import google.generativeai as genai
+
+from PyPDF2 import PdfReader
+from IPython.display import display
+from IPython.display import Markdown
+
+# Used to securely store your API key
+from google.colab import userdata
+
+# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
+GOOGLE_API_KEY = userdata.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+dir_path = os.getcwd()
+print(f"path: {dir_path}")
+
+# Function to pre-process CV (replace with your logic)
+def preprocess_cv(cv_file):
+  print(f"cv file: {cv_file}")
+
+  with open(cv_file, 'rb') as file:
+    pdf_reader = PdfReader(file)
+
+    # Extract text from all pages (replace with page range if needed)
+    cv_text = ""
+    for page_num in range(len(pdf_reader.pages)):
+      page = pdf_reader.pages[page_num]
+      cv_text += page.extract_text()
+
+  return cv_text
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+def parse_cv_with_gemini(uploaded_file):
+  model = genai.GenerativeModel('gemini-1.5-flash')  
+  cv_text = preprocess_cv(uploaded_file)
+
+  purpose_of_text = "Analyze the candidate by their CV in the text format, return their main information, skills, and what are the main expertise of the candidate. CV is here: " 
+  response = model.generate_content(purpose_of_text + cv_text)
+  return to_markdown(response.text)
+
+
+uploaded_file = "./sample_data/Shadman_Jamil_CV_2024_v2.1.pdf"
+parse_cv_with_gemini(uploaded_file)
+
+#other help 
+#response.prompt_feedback
+#response.candidates
+#https://colab.research.google.com/github/google/generative-ai-docs/blob/main/site/en/gemini-api/docs/get-started/python.ipynb#scrollTo=5b4Hkfj-pm3p
